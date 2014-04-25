354760cae6e851fe29c6de1a392a80a158ec824d
d <dhenton@localhost.com>
2014-04-25 14:03:44 -0500

    service admin system uses jpa/database for storing of service registry

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dhenton
+ */
+import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
+//import org.hibernate.ejb.HibernatePersistence;
+//import org.hibernate.engine.spi.SessionOwner;
+public class NewClass {
+    
+    Logger log;
+}
